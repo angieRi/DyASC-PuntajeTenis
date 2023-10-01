d1292894ea74ff4ae8329015697be1ed777b1b95
Alex Lovera <45718259+AlexLovera@users.noreply.github.com>
2023-09-30 22:56:16 -0300

    Elimina codigo de sumarPuntos en Marcador

--- src/main/java/untref/dysac/Tenis/Marcador.java
+++ src/main/java/untref/dysac/Tenis/Marcador.java
@@ -11,28 +11,6 @@ public class Marcador {
     public Marcador(List<Jugador> jugadores) {
         this.jugadores = jugadores;
     }
-    void sumarPuntos(Jugador jugador){
-        //Jugador jugador = this.jugadores.;
-
-        switch (jugador.getPuntaje()){
-            case 0:
-                jugador.setPuntaje(15);
-                break;
-            case 15:
-                jugador.setPuntaje(30);
-                break;
-            case 30:
-                jugador.setPuntaje(40);
-                break;
-            case 40:
-                // deberia chequear si hay ventajas
-                // dependiendo de cual tenga ventaja se le saca al otro O gana el game
-                // si gana un game puede ganar un set-> posibilidad de tie break
-                // si gana 3 sets gana el partido?
-                jugador.setPuntaje(0);
-                break;
-        }
-    }
     public Object getMarcador(Jugador jugadorEquipoA) {
         return jugadorEquipoA.getPuntaje();
     }
