cbea57cd188acc3f98c99149a0fdc21556de6a65
Alex Lovera <45718259+AlexLovera@users.noreply.github.com>
2023-09-30 22:15:19 -0300

    Arregla test para constructor de marcador

--- src/test/java/untref/dysac/Tenis/MarcadorTest.java
+++ src/test/java/untref/dysac/Tenis/MarcadorTest.java
@@ -2,6 +2,8 @@ package untref.dysac.Tenis;
 
 import org.junit.Test;
 
+import java.util.*;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
@@ -9,16 +11,22 @@ public class MarcadorTest {
     @Test
     public void iniciaMarcadoryMuestraPuntosDeJugador()
     {
-        Jugador jugadorEquipoA = new Jugador();
-        Marcador juegoTenis = new Marcador(jugadorEquipoA);
+        Jugador jugadorEquipoA = new Jugador("as");
+        List<Jugador> jugadores = new ArrayList<>();
+        jugadores.add(new Jugador("jugador 1"));
+        jugadores.add(new Jugador("jugador 2"));
+        Marcador juegoTenis = new Marcador(jugadores);
         assertEquals(0,juegoTenis.getMarcador(jugadorEquipoA));
         assertNotEquals(15,juegoTenis.getMarcador(jugadorEquipoA));
     }
     @Test
     public void marcadorConseguirGame()
     {
-        Jugador jugadorEquipoA = new Jugador();
-        Marcador juegoTenis = new Marcador(jugadorEquipoA);
+        Jugador jugadorEquipoA = new Jugador("as");
+        List<Jugador> jugadores = new ArrayList<>();
+        jugadores.add(new Jugador("jugador 1"));
+        jugadores.add(new Jugador("jugador 2"));
+        Marcador juegoTenis = new Marcador(jugadores);
      //   assertEquals(0,juegoTenis.getGames());
         assertNotEquals(15,juegoTenis.getMarcador(jugadorEquipoA));
     }
